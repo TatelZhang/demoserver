82a932777342ae643ca558298a57a4499871282c
Tatel <782487339@qq.com>
2017-11-06 23:02:16 +0800

    cookie 保存登录状态。canvas 验证码

--- controllers/api.js
+++ controllers/api.js
@@ -7,18 +7,32 @@ var login = async(ctx, next)=>{
         username = ctx.request.body.username,
         passwd = ctx.request.body.passwd,
         usernameByCookie = ctx.cookies.get('username');
-    if((username==="tatel"&&passwd === '123456')|| (usernameByCookie===username&&username==='tatel')){
-        ctx.response.status = 200;
-        ctx.response.body = { text: `hello, ${username}`, username: usernameByCookie};
-        if(usernameByCookie)ctx.cookies.set('username', 'tatel');
-    }else{
-        ctx.response.status = 404;
-        ctx.response.body = `用户名 ${username} 不存在，或密码错误`;
-    }
+        if(usernameByCookie==="tatel"){ // cookie登录
+            ctx.status = 200;
+            ctx.body = { account: { username: usernameByCookie, avatar: '/static/doge.png', isLogin: true},};
+        }else if (username === "tatel" && passwd === '123456'){ // 用户名密码登录
+            ctx.status = 200;
+            var expiresdate = new Date();
+            expiresdate.setDate(new Date().getDate() + 1);
+            ctx.cookies.set("username", username, {
+                expires: expiresdate
+            });
+            ctx.body = { account: { username: username, avatar: '/static/doge.png', isLogin: true}};
+        }else{
+            ctx.status = 404;
+            ctx.response.body = { account: {isLogin:false}, errorText: `用户名${username}不存在, 或密码错误`};
+        }
 }
 
+var loginOut = async(ctx, next)=>{
+    var usernameByCookie = ctx.cookies.get("username");
+    ctx.cookies.set('username', '', {expires: new Date(0)});
+    ctx.status = 200;
+    ctx.response.body = { account: { isLogin: false }};
+}
 
 module.exports = {
     "GET /api": api,
-    "POST /api/login": login
+    "POST /api/login": login,
+    "GET /api/login": loginOut
 }
--- controllers/home.js
+++ controllers/home.js
@@ -3,7 +3,7 @@ const config = require('../common.conf')
 
 var home = async (ctx, next)=>{
     ctx.response.status = 200;
-    console.log(ctx.req)
+    // console.log(ctx.req)
     ctx.response.body = '<h1> hello</h1>';
     // ctx.cookies.set("username", "tatel",{
     // })
@@ -11,7 +11,7 @@ var home = async (ctx, next)=>{
     // console.log(us);
     // console.log(ctx.cookies);
     // console.log(typeof ctx.cookies);
-    // ctx.render('index.html');
+    ctx.render('index.html');
 };
 
 var hello = async (ctx, next)=>{
